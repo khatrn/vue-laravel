9a72578bf6f8e37b0313d1a3d7c7dc795128e980
Kha Tran <kha.tn@neo-lab.vn>
2021-03-24 14:01:45 +0700

    Lesson 2: Move message to script

--- learn-vue/resources/js/components/MyComponent.vue
+++ learn-vue/resources/js/components/MyComponent.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="my-component">Đây là component đầu tiên của tôi</div>
+    <div class="my-component">{{ message }}</div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            message: 'Đây là component đầu tiên của tôi'
+        }
+    }
 }
 </script>
 
