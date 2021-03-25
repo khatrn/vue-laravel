3612dd9106cf31e77faab95fdb5be822ab8861aa
Kha Tran <kha.tn@neo-lab.vn>
2021-03-25 15:53:38 +0700

    Lession 9: Communication Components

---
+++ learn-vue/resources/js/components/ListUser.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="list-user">
+        <table class="tbl-list-user">
+            <tr>
+                <th>Tên</th>
+                <th>Vị trí</th>
+                <th>DOB</th>
+            </tr>
+            <tr v-for="user in userData">
+                <td><a @click="selectUser(user)">{{ user.name.title }} - {{ user.name.last }} - {{ user.name.first }}</a></td>
+                <td>{{ user.location.city }}</td>
+                <td>{{ user.dob.date }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+import data from '../data.json'
+export default {
+    data() {
+        return {
+            userData: data,
+            number: 1998,
+            string: 'HTD'
+        }
+    },
+    methods: {
+        selectUser(user) {
+            this.$emit('userSelected', user, this.number, this.string)
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .tbl-list-user{
+        width: 100%;
+    }
+</style>
---
+++ learn-vue/resources/js/components/UserDetail.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="user-detail-container" v-if="Object.keys(userDetailInfo).length !== 0">
+        <div class="user-name">
+            Tên: {{ userDetailInfo.name.last }} {{ userDetailInfo.name.first }}
+        </div>
+        <div class="user-email">
+            Email: {{ userDetailInfo.email }}
+        </div>
+        <div class="user-dob">
+            DOB: {{ userDetailInfo.dob.date }}
+        </div>
+        <div class="user-gender">
+            Giới tính: {{ userDetailInfo.gender }}
+        </div>
+        <div class="user-address">
+            Địa chỉ: {{ userDetailInfo.location.city }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        userDetailInfo: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
